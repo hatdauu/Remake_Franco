637fb7145403c03e9237301c23c4d08ad27093cd
Nguyen Son <hatdau2k2@gmail.com>
2023-03-07 15:22:17 +0700

    upload

--- slick.js
+++ slick.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         arrows: true,
         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
-        // dots: true
+        dots: true,
         pauseOnHover: false,
         responsive: [
             {
